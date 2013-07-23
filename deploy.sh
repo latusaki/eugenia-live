4fa06d1b3c9e2f8e355869274ea787dbc358366e
latusaki <il555@york.ac.uk>
2013-07-24 00:05:13 +0100

    saveJSON broken to update/create

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Building static application with Hem"
-node_modules/hem/bin/hem build
+hem build
 git add public/application.{css,js}
 git commit -m "Building for deployment"
 echo "Pushing to Heroku"
-git push heroku
+git push heroku master
